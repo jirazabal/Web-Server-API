4eefb117727105ed5f3e0d3e9adc083578ced3bd
Justin Irazabal <irazabal@udel.edu>
2018-10-05 14:48:58 -0400

    changed to subdocs

--- node-rest-api/models/Course.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var CourseSchema = new mongoose.Schema({
-    course: Number,   
-    updated_at: {type: Date, default: Date.now},
-});
-
-module.exports = mongoose.model('Course', CourseSchema);
--- node-rest-api/models/Questions.js
+++ node-rest-api/models/Questions.js
@@ -1,16 +1,27 @@
 var mongoose = require('mongoose');
-var course = require('Course.js');
-var slo = require('SLO.js');
+
+var SLOSchema = new mongoose.Schema({
+    skill_SLO: String,
+    unit: String,
+    topic: String,    
+    updated_at: {type: Date, default: Date.now},
+});
+
+var CourseSchema = new mongoose.Schema({
+    course: Number,   
+    updated_at: {type: Date, default: Date.now},
+});
 
 var QuestionsSchema = new mongoose.Schema({
-    course_id: course,
+    course_id: [CourseSchema],
     question_type: String,
     question_text: String,
     answer_choices: Array,
     none_above: Boolean,
     all_above: Boolean,
     difficulty: String,
-    skill_SLO: slo,
+    cognitive_level: String,
+    skill_SLO: [SLOSchema],
     
     updated_at: {type: Date, default: Date.now},
 });
