80a4bbdc3e8659ff3ab8dcc7026380f5e89ec668
Justin Irazabal <irazabal@udel.edu>
2018-10-09 09:37:21 -0400

    updated schemas

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var products = require('./routes/products');
+var questions = require('./routes/questions');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', products);
+app.use('/questions', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
