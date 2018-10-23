bf99e398af3bff6f3aaa174ceac4ee96aec7da27
Justin Irazabal <irazabal@udel.edu>
2018-10-23 16:20:09 -0400

    edited app.js

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var products = require('./routes/products');
 var questions = require('./routes/questions');
 
 var indexRouter = require('./routes/index');
@@ -30,7 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/products', products);
 app.use('/questions', questions);
 
 // catch 404 and forward to error handler
